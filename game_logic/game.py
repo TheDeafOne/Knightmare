25495002376efb8c112ac454bdabc5cdfdf7c443
TheDeafOne <woodburnkb20@gcc.edu>
2023-04-19 22:41:22 -0400

    more project structure

--- game_logic/game.py
+++ game_logic/game.py
@@ -4,6 +4,7 @@ from board_utils import BoardConstants as constants
 class Chess:
     def __init__(self):
         board = Board()
+        player = 0
         #TODO: gameloop
             # prompt player for move (draw or win)
             # check for endgame conditions
