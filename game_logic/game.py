38d11172c49937d5f4846e9d4fdc019902fde56b
TheDeafOne <woodburnkb20@gcc.edu>
2023-04-23 16:03:56 -0400

    player color indication set up

--- game_logic/game.py
+++ game_logic/game.py
@@ -18,6 +18,7 @@ class Chess:
         self.BLACK = (0, 0, 0)
         self.WHITE = (255, 255, 255)
         self.GRAY = (128, 128, 128)
+        self.EGGSHELL = (245, 245, 245)
 
         self.WINDOW_SIZE = (425, 425)
         self.PIECE_IMAGE_SIZE = (45, 45)
@@ -60,6 +61,8 @@ class Chess:
         # game states
         self.game_state = "start_menu"
         self.player_state = "selection"
+        self.player_moves = []
+        self.player_focus = None
 
         # Create the window
         pygame.init()
@@ -92,22 +95,37 @@ class Chess:
                     
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         mouse_position = pygame.mouse.get_pos()
-                        tentative_moves = []
                         position = self.get_square_clicked(mouse_position)
                         print(position)
                         if position != None:
                             piece = self.board.get_piece(position)
-                        if self.player_state == "selection":
-                            if position != None and self.board.get_piece_color(piece) == self.player:
-                                self.game_state = "move"
-                                moves = self.board.board_to_piece_list(
-                                    self.board.get_moves(position))
-                                tentative_moves = moves
-                                for move in moves:
-                                    row, col = utils.square_to_row_col(move)
-                                    self.highlight_square(row, col)
-                        # if self.player_state == "move"
-                                
+                            if self.player_state == "selection":
+                                if self.board.get_piece_color(piece) == self.player:
+                                    self.player_state = "move"
+                                    self.player_focus = position
+                                    self.player_moves = self.board.board_to_piece_list(
+                                        self.board.get_moves(position))
+                                    
+                            if self.player_state == "move":
+                                if position not in self.player_moves:
+                                    self.player_focus = position
+                                    self.player_moves = self.board.board_to_piece_list(
+                                        self.board.get_moves(position))
+                                else:
+                                    # move piece
+                                    self.board.move_piece(self.player_focus, position)
+
+                                    # switch players
+                                    tmp_p = self.player
+                                    self.player = self.opponent
+                                    self.opponent = tmp_p
+
+                                    # switch player state
+                                    self.player_moves = []
+                                    self.player_focus = None
+                                    self.player_state = "selection"
+
+
 
             # Update the display
             pygame.display.update()
@@ -150,7 +168,8 @@ class Chess:
 
     def draw_game(self):
         # Draw the chess board
-        for row in range(8):
+        index = 0
+        for row in range(7,-1,-1):
             for col in range(8):
                 x = col * self.SQUARE_SIZE + self.SQUARE_SIZE//2
                 y = row * self.SQUARE_SIZE
@@ -162,10 +181,11 @@ class Chess:
                                  x, y, self.SQUARE_SIZE, self.SQUARE_SIZE])
 
                 # draw pieces
-                piece = self.draw_board[row][col]
-                if piece != "":
+                piece = self.board.get_piece(index)
+                if piece != constants.EMPTY:
                     image = self.piece_map[piece]
                     self.window.blit(image, (x, y))
+                index += 1
 
         index_font = pygame.font.SysFont("Arial", int(self.SQUARE_SIZE/2.5))
         for i in range(8):
@@ -182,15 +202,24 @@ class Chess:
         
         current_player = 'W'
         current_color = self.WHITE
-        contrast_color = self.BLACK
+        contrast = 0
+        letter_pos = 8
         if self.player == self.board.black_pieces:
             current_player = 'B'
             current_color = self.BLACK
-            contrast_color = self.WHITE
-        player_indication_position = (self.SQUARE_SIZE/8,self.WINDOW_SIZE[0]-self.SQUARE_SIZE/2)
-        pygame.draw.circle(self.window, contrast_color, (self.SQUARE_SIZE/3.5,self.WINDOW_SIZE[0]-self.SQUARE_SIZE/4), self.SQUARE_SIZE/4)
+            contrast = 2
+            letter_pos = 6
+
+        player_indication_position = (self.SQUARE_SIZE/letter_pos,self.WINDOW_SIZE[0]-self.SQUARE_SIZE/2)
+        pygame.draw.circle(self.window, self.WHITE, (self.SQUARE_SIZE/3.5,self.WINDOW_SIZE[0]-self.SQUARE_SIZE/4), self.SQUARE_SIZE/4)
+        pygame.draw.circle(self.window, self.BLACK, (self.SQUARE_SIZE/3.5,self.WINDOW_SIZE[0]-self.SQUARE_SIZE/4), self.SQUARE_SIZE/4, contrast)
         player_label = index_font.render(current_player, True, current_color)
         self.window.blit(player_label, player_indication_position)
+
+        if self.player_moves:
+            for move in self.player_moves:
+                row, col = utils.square_to_row_col(move)
+                self.highlight_square(col, row)
 
 
 
@@ -210,14 +239,11 @@ class Chess:
 
     # https://stackoverflow.com/questions/6339057/draw-a-transparent-rectangles-and-polygons-in-pygame
     def highlight_square(self, x, y):
-        left = (x * self.SQUARE_SIZE) + (self.SQUARE_SIZE // 2 + self.SQUARE_SIZE)
-        top = (y * self.SQUARE_SIZE) - self.SQUARE_SIZE // 2
-
-        # square_surface = pygame.Surface((top, left), pygame.SRCALPHA)
-        # pygame.draw.rect(self.window, (0, 0, 255, 125), rect)
-
+        y = self.BOARD_SIZE - y - 1
+        left = (x * self.SQUARE_SIZE) + self.SQUARE_SIZE/2
+        top = (y * self.SQUARE_SIZE)
         highlight_surface = pygame.Surface(
             (self.SQUARE_SIZE, self.SQUARE_SIZE), pygame.SRCALPHA)  # the size of your rect
         # this fills the entire surface
         highlight_surface.fill((173, 216, 230, 150))
-        self.window.blit(highlight_surface, (top, left))
+        self.window.blit(highlight_surface, (left, top))
